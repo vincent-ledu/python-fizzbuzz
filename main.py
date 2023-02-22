f601031d464180ad5a333152eb144d39d7a5a1ec
Vincent Le Dû <vincent.ledu@gmail.com>
2023-02-22 15:38:17 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def inc(x):
+  return x + 1
+
+
+if __name__ == "__main__":
+    import sys
+    print(inc(int(sys.argv[1])))
---
+++ main_test.py
@@ -0,0 +1,10 @@
+import main
+
+def test_inc():
+  assert main.inc(3) == 5
+
+
+if __name__ == "__main__":
+    import sys
+    print("hop")
+    test_inc()
